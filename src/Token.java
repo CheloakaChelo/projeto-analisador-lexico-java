04b8385049fa4b45d08caf4d8b771c5d83476cec
rBasstos <rafaeltorresbastos@gmail.com>
2024-11-07 11:24:14 -0300

    teste

--- src/Token.java
+++ src/Token.java
@@ -1,45 +1,45 @@
-import listatokens.TokenName;
+    import listatokens.TokenName;
 
-public class Token {
+    public class Token {
 
-    private int numero;
+        private int numero;
 
-    private TokenName tipo;
+        private TokenName tipo;
 
-    private String token;
+        private String token;
 
-    public Token(int numero, TokenName tipo, String token) {
-        this.numero = numero;
-        this.tipo = tipo;
-        this.token = token;
+        public Token(int numero, TokenName tipo, String token) {
+            this.numero = numero;
+            this.tipo = tipo;
+            this.token = token;
+        }
+
+        public int getNumero() {
+            return numero;
+        }
+
+        public void setNumero(int numero) {
+            this.numero = numero;
+        }
+
+        public TokenName getTipo() {
+            return tipo;
+        }
+
+        public void setTipo(TokenName tipo) {
+            this.tipo = tipo;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + numero + "-> "+ tipo +"-> "+ token +")";
+        }
     }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public TokenName getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TokenName tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + numero + "-> "+ tipo +"-> "+ token +")";
-    }
-}
